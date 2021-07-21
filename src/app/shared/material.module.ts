9eb4a0844bee9740c5e2300b8418a817ba8d7998
david20571015 <david20571015@gmail.com>
2021-07-21 17:57:12 +0800

    Update: Rough UI of homepage.

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatSidenavModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatSidenavModule
+  ]
+})
+export class MaterialModule { }
