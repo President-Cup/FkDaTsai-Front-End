c80bd0f8381deb4f530a507af8f6e7bc195342e9
david20571015 <david20571015@gmail.com>
2021-07-21 15:07:14 +0800

    Update: Move component files

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './page/home/home.component';
+import { HomeComponent } from './page/home.component';
 import { ItemCardComponent } from './page/item-card/item-card.component';
 
 
--- src/app/modules/upload/upload.module.ts
+++ src/app/modules/upload/upload.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UploadComponent } from './page/upload/upload.component';
+import { UploadComponent } from './page/upload.component';
 
 
 
