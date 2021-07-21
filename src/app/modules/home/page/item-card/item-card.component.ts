565b08e4edd9904b1a2992135b25ee2f68a1c945
david20571015 <david20571015@gmail.com>
2021-07-22 03:07:56 +0800

    Update: Temporary item-card.

--- src/app/modules/home/page/item-card/item-card.component.ts
+++ src/app/modules/home/page/item-card/item-card.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ItemService } from '@data/service/item.service';
+
+import { Item } from '@data/types/item';
 
 @Component({
-  selector: 'app-item-card',
+  selector: 'app-item-card [item]',
   templateUrl: './item-card.component.html',
   styleUrls: ['./item-card.component.scss']
 })
 export class ItemCardComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  itemId!: number;
+
+  item?:  Item;
+
+  constructor(private itemService: ItemService) { }
 
   ngOnInit(): void {
+    this.itemService.fetchItem(this.itemId)
+      .then((item) => {
+        this.item = item;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
 }
