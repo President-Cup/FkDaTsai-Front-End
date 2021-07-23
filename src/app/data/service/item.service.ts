3a9036fa080ce069c82369b09c98a0ccb81864e5
david20571015 <david20571015@gmail.com>
2021-07-23 18:29:59 +0800

    Update: Update fetch functions.

--- src/app/data/service/item.service.ts
+++ src/app/data/service/item.service.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Item } from '@data/types/item';
+
+import { API_URL } from '@core/constants/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor() { }
+  private itemUrl = `${API_URL}/item`;
 
-  async fetchItem(itemId: number): Promise<Item> {
-    return new Promise<Item>((resolve, reject) => { })
+  constructor(private http: HttpClient) { }
+
+  async fetchNumberOfItems(category_name: [string, string]): Promise<number> {
+    return this.http.get<number>(`${this.itemUrl}?q=number&primary=${category_name[0]}&secondary=${category_name[1]}`)
+      .toPromise();
   }
 
-  async listItemIdByCategory(main: string, minor: string): Promise<number[]> {
-    return new Promise<number[]>((resolve, reject) => { });
+  async fetchItem(itemId: number): Promise<Item> {
+    return this.http.get<Item>(`${this.itemUrl}?q=item&id=${itemId}`)
+      .toPromise();
+  }
+
+  async fetchItemIdList(category_name: [string, string], pageIndex: number, pageSize: number): Promise<number[]> {
+    return this.http.get<number[]>(`${this.itemUrl}?q=list&category=${category_name}&page=${pageIndex}&per_page=${pageSize}`)
+      .toPromise();
   }
 }
