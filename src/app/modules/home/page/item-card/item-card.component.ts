3bd456b993d4667c172159ea88903fa1606223d2
david20571015 <david20571015@gmail.com>
2021-07-25 14:53:04 +0800

    Update: Add likes button.

--- src/app/modules/home/page/item-card/item-card.component.ts
+++ src/app/modules/home/page/item-card/item-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+
 import { ItemService } from '@data/service/item.service';
+import { LikesListService } from '@data/service/likes-list.service';
 
 import { Item } from '@data/types/item';
 
@@ -10,21 +12,26 @@ import { Item } from '@data/types/item';
 })
 export class ItemCardComponent implements OnInit {
 
-  @Input()
-  itemId!: number;
+  @Input() itemId!: number;
 
-  item?:  Item;
+  item?: Item;
 
-  constructor(private itemService: ItemService) { }
+  constructor(
+    private itemService: ItemService,
+    public likesListService: LikesListService
+  ) { }
 
   ngOnInit(): void {
     this.itemService.fetchItem(this.itemId)
-      .then((item) => {
-        this.item = item;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+      .then((item) => this.item = item)
+      .catch((error) => console.error(error));
   }
 
+  toggleLikesStatus(): void {
+    if (this.likesListService.isLiked(this.itemId)) {
+      this.likesListService.dislike(this.itemId);
+    } else {
+      this.likesListService.like(this.itemId);
+    }
+  }
 }
