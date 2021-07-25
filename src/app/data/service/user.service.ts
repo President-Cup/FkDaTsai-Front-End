a1cb80743ccd4c4d3a33d0636ecddd5010780167
david20571015 <david20571015@gmail.com>
2021-07-25 23:34:11 +0800

    Update: Implement user class and service.

--- src/app/data/service/user.service.ts
+++ src/app/data/service/user.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '@data/types/user';
+
+import { API_URL } from '@core/constants/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private _isLogin = false;
+  get isLogin() { return this._isLogin; }
+
+  private _user?: User;
+  get user() { return this._user; }
+
+  constructor(private http: HttpClient) { }
+
+  isValid(name: string, email: string, password: string) {
+    if (!name || !name.trim() || !name.match('/^\w+$/')) {
+      return false;
+    }
+    if (!email || !email.trim() || !email.match('/^\w+((-\w+)|(\.\w+))*\@\w+((\.|-)\w+)*\.[A-Za-z]+$/')) {
+      return false;
+    }
+    if (!password || !password.trim() || password.length < 8 || !password.match('/^\w+$/')) {
+      return false;
+    }
+    return true;
+  }
+
+  async signUp(name: string, email: string, password: string) {
+    let body = {
+      name: name,
+      email: email,
+      password: password
+    };
+
+    return this.http.post<User>(`${API_URL}/signup`, body)
+      .toPromise();
+  }
+
+  async signin(email: string, password: string) {
+    let body = {
+      email: email,
+      password: password
+    };
+
+    return this.http.post<User>(`${API_URL}/signin`, body)
+      .toPromise();
+  }
 }
--- src/app/data/types/user.ts
+++ src/app/data/types/user.ts
@@ -1,2 +1,18 @@
 export class User {
+
+    constructor(
+        readonly id: number,
+        readonly name: string,
+        readonly email: string,
+        readonly itemIdList: number[],
+    ) { }
+
+    addItem(id: number): void {
+        this.itemIdList.push(id);
+    }
+
+    removeItem(id: number): void {
+        let index = this.itemIdList.indexOf(id);
+        this.itemIdList.splice(index, 1);
+    }
 }
